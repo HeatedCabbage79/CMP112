19445a2df97c50656539faa930ebc1d4940d5d10
HeatedCabbage79 <2501646@uad.ac.uk>
2025-12-04 17:08:10 +0000

    Trying to get movement
    Trying to make a platform rotate according to player inputs not working

---
+++ Assets/Scipts/Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerTarget : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
+    {
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
+
+    }
+}
---
+++ Assets/Scipts/Rotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform target;
+    public float turnSpeed;
+    Quaternion rotGoal;
+    Vector3 direction;
+    void Update()
+    {
+        direction = (target.position - transform.position).normalized;
+        rotGoal = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotGoal, turnSpeed);
+    }
+}
